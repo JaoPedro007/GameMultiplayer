50a69788fe6fa6caf75fdec82ba9cbcdaac11ab6
JaoPedro007 <joaopedrorodriguesleite9@gmail.com>
2024-05-16 00:20:29 +0000

    Adjusting to deploy

---
+++ dist/keyboard-listener.js
@@ -0,0 +1,32 @@
+export default function createKeyboardListener(document) {
+  const state = {
+    observers: [],
+    playerId: null
+  };
+  function registerPlayerId(playerId) {
+    state.playerId = playerId;
+  }
+  function subscribe(observerFunction) {
+    state.observers.push(observerFunction);
+  }
+  function notifyAll(command) {
+    console.log(`Nofiying ${state.observers.length} observers`);
+    for (const observerFunction of state.observers) {
+      observerFunction(command);
+    }
+  }
+  document.addEventListener('keydown', handleKeyDown);
+  function handleKeyDown(event) {
+    const keyPressed = event.key;
+    const command = {
+      type: 'move-player',
+      playerId: state.playerId,
+      keyPressed
+    };
+    notifyAll(command);
+  }
+  return {
+    subscribe,
+    registerPlayerId
+  };
+}
